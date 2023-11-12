0d7f33c4569191c49316f71b3aeb22a7c68cc52e
john ndichu <johnndichu104@gmail.com>
2023-11-11 17:40:42 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+let hamburger=document.querySelector(".hamburger");
+       let nav = document.querySelector(".nav");
+       hamburger.onclick = function(){
+     
+       nav.classList.toggle("active");
+
+       }
