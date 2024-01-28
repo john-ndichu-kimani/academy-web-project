f8c61d43f5eb0439df341ff3e1a57b9ee8998a18
john ndichu <johnndichu104@gmail.com>
2024-01-28 09:35:41 +0300

    completed the whole project

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+
+
+
+
 let hamburger=document.querySelector(".hamburger");
        let nav = document.querySelector(".nav");
        hamburger.onclick = function(){
@@ -5,3 +9,33 @@ let hamburger=document.querySelector(".hamburger");
        nav.classList.toggle("active");
 
        }
+
+ //scroll
+ 
+ document.addEventListener("DOMContentLoaded", function () {
+       // Smooth scroll for navigation links
+       const navLinks = document.querySelectorAll('a[href^="#"]');
+       
+       navLinks.forEach(link => {
+           link.addEventListener('click', smoothScroll);
+       });
+   
+       function smoothScroll(e) {
+           e.preventDefault();
+   
+           const targetId = this.getAttribute('href').substring(1);
+           const targetElement = document.getElementById(targetId);
+   
+           if (targetElement) {
+               const offsetTop = targetElement.offsetTop;
+   
+               window.scrollTo({
+                   top: offsetTop,
+                   behavior: 'smooth'
+               });
+           }
+       }
+   });
+
+   
+   
